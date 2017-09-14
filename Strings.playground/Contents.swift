be81c5cf5cb84ba4ca0aee80e1b7be5c71569431
Pablo Osinaga <paguos@gmail.com>
2017-09-14 14:42:29 +0200

    Add Strings.playground

---
+++ Strings.playground/Contents.swift
@@ -0,0 +1,19 @@
+//: Playground - noun: a place where people can play
+
+import Cocoa
+
+var str = "Hello, playground!"
+var empt = ""
+
+str + " Welcome to swift!"  //"Concatenates two strings"
+str.characters.count        //"Size of the string"
+str.capitalized             //"Capitalize string"
+str.lowercased()            //"Lowercase a string"
+str.uppercased()            //"Uppercase a string"
+
+str.isEmpty                 //"Is a string empty?"
+empt.isEmpty
+
+str.hasPrefix("Hello")      //"Checks if a text appears at the beginning"
+str.hasSuffix("!")          //"Checks if a text appears at the end"
+
