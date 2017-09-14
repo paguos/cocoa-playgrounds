6b2eaa54423c3c352755c098b4c797a4dcce0a34
Pablo Osinaga <paguos@gmail.com>
2017-09-14 14:32:52 +0200

    Add Numbers.playground

---
+++ Numbers.playground/Contents.swift
@@ -0,0 +1,56 @@
+//: Playground - noun: a place where people can play
+
+import Cocoa
+
+/* Rounding Functions */
+
+var numb = 5.54
+
+round(numb) //“Rounds up from 0.5 and higher and rounds down from 0.49 and lower”
+floor(numb) //“Rounds to the lowest integer”
+ceil(numb)  //“Rounds to the highest integer”
+trunc(numb) //“Drops the decimal value”
+
+/* Calculation Functions */
+
+fabs(-20)       //"Absolute Value"
+sqrt(25)        //"Square Root"
+cbrt(27.0)      //"Cube Root"
+hypot(3.0,4.0)  //“Square Root of (x*x + y*y)”
+fmax(24.3, 3.4) //"Maximum of 2 values"
+fmin(24.3, 3.4) //"Minumum of 2 values"
+
+/* Trigonometry Functions */
+
+numb = numb - 5
+
+sin(numb)
+cos(numb)
+tan(numb)
+
+sinh(numb)
+cosh(numb)
+tanh(numb)
+
+asin(numb)
+acos(numb)
+atan(numb)
+
+asinh(numb)
+acosh(numb)
+atan(numb)
+
+/* Exponential Functions */
+
+exp(numb)       //"eˆx"
+exp2(numb)      //"2ˆx"
+__exp10(numb)   //"10ˆx"
+expm1(numb)     //"eˆ(x-1)"
+pow(2,3)
+
+/* Logarithmic Functions */
+
+log(3.0)        //"Natural logarithm"
+log2(3.0)       //"Base-2 logarithm"
+log10(3.0)      //"Base-10 logarithm"
+log1p(3.0)      //"Natural log of 1 + x"
